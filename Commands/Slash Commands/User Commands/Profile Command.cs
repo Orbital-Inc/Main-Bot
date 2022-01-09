2b50417d2513d1f8f8545e65f6df62237fe40c18
Nebula <nebula@nebulamods.ca>
2022-01-09 19:25:40 -0400

    ADDED profile command to view account information for discord & our site

---
+++ Commands/Slash Commands/User Commands/Profile Command.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Interactions;
+
+namespace Main_Bot.Commands.Slash_Commands.User_Commands;
+
+public class ProfileCommand : InteractionModuleBase<ShardedInteractionContext>
+{
+    [SlashCommand("profile", "Display details about your account.")]
+    public async Task ViewProfile(IUser? user = null)
+    {
+        //finish command later.
+        await Task.CompletedTask;
+    }
+}
