062037902896e42eaf9cdb26637ef403403ee11e
nebula <nebula@nebulamods.ca>
2022-03-06 14:41:52 -0400

    - Staged error log into new database layout

---
+++ Database/Models/Logs/Error Log.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MainBot.Database.Models.Logs;
+
+public class ErrorLog
+{
+    [Key]
+    public int key { get; set; }
+    public DateTime errorTime { get; set; }
+    public string? source { get; set; }
+    public string? message { get; set; }
+    public string? stackTrace { get; set; }
+    public string? extraInformation { get; set; }
+}
