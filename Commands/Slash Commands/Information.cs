1074a8c896e64b860e8119e10588edd64e8b611b
nebula <nebula@nebulamods.ca>
2022-07-01 19:02:43 -0300

    - Updated information command to display amount of members in guild & asn information - Updated version to 1.0.0.4

--- Commands/Slash Commands/Information.cs
+++ Commands/Slash Commands/Information.cs
@@ -13,7 +13,9 @@ public class InformationCommand : InteractionModuleBase<ShardedInteractionContex
         Discord.Rest.RestApplication? appInfo = await Context.Client.GetApplicationInfoAsync();
         await Context.ReplyWithEmbedAsync("Information",
             $"Guild Count: {Context.Client.Guilds.Count}\n" +
+            $"Guild Member Count: {Context.Guild.MemberCount}\n" +
             $"Developer: {(Context.Guild is null ? $"{appInfo.Owner.Username}#{appInfo.Owner.Discriminator}" : appInfo.Owner.Mention)}\n" +
+            $"Nebula Mods, Inc. ASN: [AS397441](https://asn.ipinfo.app/AS397441)" +
             $"Uptime: <t:{((DateTimeOffset)Process.GetCurrentProcess().StartTime).ToUnixTimeSeconds()}:R>\n" +
             $"Build Version: {Assembly.GetExecutingAssembly().GetName().Version}\n" +
             $"{appInfo.Description}\n" +
